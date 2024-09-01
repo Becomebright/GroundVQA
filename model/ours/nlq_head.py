7aceb17d1a7dab126e46335a8c5f0dbfa454c111
Bright <dishangzhe@sjtu.edu.cn>
2024-09-01 15:12:37 +0800

    Remove duplicate LayerNorm class in nlq_head.py

--- model/ours/nlq_head.py
+++ model/ours/nlq_head.py
@@ -323,52 +323,6 @@ class MaskedConv1D(nn.Module):
         return out_conv, out_mask
 
 
-class LayerNorm(nn.Module):
-    """
-    LayerNorm that supports inputs of size B, C, T
-    """
-
-    def __init__(
-            self,
-            num_channels,
-            eps=1e-5,
-            affine=True,
-            device=None,
-            dtype=None,
-    ):
-        super().__init__()
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        self.num_channels = num_channels
-        self.eps = eps
-        self.affine = affine
-
-        if self.affine:
-            self.weight = nn.Parameter(
-                torch.ones([1, num_channels, 1], **factory_kwargs))
-            self.bias = nn.Parameter(
-                torch.zeros([1, num_channels, 1], **factory_kwargs))
-        else:
-            self.register_parameter('weight', None)
-            self.register_parameter('bias', None)
-
-    def forward(self, x):
-        assert x.dim() == 3
-        assert x.shape[1] == self.num_channels
-
-        # normalization along C channels
-        mu = torch.mean(x, dim=1, keepdim=True)
-        res_x = x - mu
-        sigma = torch.mean(res_x ** 2, dim=1, keepdim=True)
-        out = res_x / torch.sqrt(sigma + self.eps)
-
-        # apply weight and bias
-        if self.affine:
-            out *= self.weight
-            out += self.bias
-
-        return out
-    
-
 class PtTransformerClsHead(nn.Module):
     """
     1D Conv heads for classification
